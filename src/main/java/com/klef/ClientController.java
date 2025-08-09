76022b8b4038895d6d6dca28bbd0b0c3ab17f125
SL <loehit2804@gmail.com>
2025-08-09 21:46:44 +0530

    updated project

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -18,10 +18,13 @@ public class ClientController
 	{
 		return "Jenkins Demo";
 	}
+	
+	@GetMapping("/test")
+	public String test()
+	{
+		return "Deploy Spring Boot using Jenkins";
+	}
 	 
-	
-	
-	
-	
+	 
 
 }
